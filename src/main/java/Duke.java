3076f8c05ad412a489bc358de47d599dd751b967
park <254471492+parkankai@users.noreply.github.com>
2026-08-29 17:42:59 +0800

    Tutorial Part 5: Tweaked the GUI

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,11 +3,16 @@ public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello!");
     }
-
+    private String commandType;
     /**
      * Generates a response for the user's chat message.
      */
     public String getResponse(String input) {
         return "Duke heard: " + input;
     }
+
+    public String getCommandType() {
+        return commandType;
+    }
+
 }
